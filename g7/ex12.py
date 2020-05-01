b493451bf31236bf756793235c01a6079a67fba9
João Figueredo <joao.eduardo37@ua.pt>
2020-05-01 11:21:10 +0100

    ex12 g7

---
+++ g7/ex12.py
@@ -0,0 +1,26 @@
+import sqlite3 as sql
+import sys
+
+def main(argv):
+    if len(argv) < 3:
+        print("Usage: python3 {} dbfile name".format(argv[0]))
+        sys.exit(0)
+    
+    db = sql.connect(argv[1])
+
+    c = 0
+
+    result = db.execute("SELECT * FROM contacts WHERE firstname LIKE ? OR middlename LIKE ? OR lastname LIKE ?",(argv[2],argv[2],argv[2],))
+    while True:
+        row = result.fetchone()
+        if not row:
+            break;
+        print("{} {} {}".format(row[1],row[2],row[3]))
+        c += 1
+    print("%d contactos"%(c))
+
+    db.close() 
+
+
+if __name__ == "__main__":
+    main(sys.argv)
